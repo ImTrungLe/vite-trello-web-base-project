686559c77f8e8fc545a12956bf9745d5be874404
trunglq <trungquangle0102@gmail.com>
2026-07-18 01:24:43 +0700

    fix: remove App.css and index.css, install material UI,

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import CSSBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CSSBaseline />
     <App />
   </React.StrictMode>,
 );
